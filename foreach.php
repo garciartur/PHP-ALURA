0b87166b684412305fb5090a83eb963cbc682f6f
Artur Garcia <arturfgarcia@gmail.com>
2020-02-02 12:06:27 -0300

    Adicionando novos itens ao array

--- foreach.php
+++ foreach.php
@@ -1,9 +1,13 @@
 <?php
 	$contascorrentes = [
-		'15988732064' => ['titular' => 'Vinícius' , 'saldo' => '1000'],
-		'25889347855' => ['titular' => 'Maria' , 'saldo' => '10000'],
-		'55722201685' => ['titular' => 'Alberto' , 'saldo' => '300'],
+		'159.887.320-64' => ['titular' => 'Vinícius' , 'saldo' => '1000'],
+		'258.893.478-55' => ['titular' => 'Maria' , 'saldo' => '10000'],
+		'557.222.016-85' => ['titular' => 'Alberto' , 'saldo' => '300'],
 	];
+
+	$contascorrentes ['222.587.710-00'] = ['titular' => 'Cláudia', 'saldo' => '2000'];
+
+	echo "Titulares:" .PHP_EOL;
 
 	foreach ($contascorrentes as $cpf => $contas){
 		echo $contas ['titular'] .PHP_EOL;
